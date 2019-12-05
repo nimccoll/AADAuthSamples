1e45cfd34b4715b65e60622282c31184aae40af4
Nicholas McCollum <nimccoll@microsoft.com>
2019-12-05 10:23:51 -0500

    Initial check-in

---
+++ Separate.API/App_Start/Startup.Auth.cs
@@ -0,0 +1,53 @@
+﻿//===============================================================================
+// Microsoft Premier Support for Developers
+// Azure Active Directory Authentication Samples
+//===============================================================================
+// Copyright © Microsoft Corporation.  All rights reserved.
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+// OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE.
+//===============================================================================
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Configuration;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Separate.API
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            WindowsAzureActiveDirectoryBearerAuthenticationOptions options = new WindowsAzureActiveDirectoryBearerAuthenticationOptions();
+
+            options.Tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = Convert.ToBoolean(ConfigurationManager.AppSettings["ida:ValidateAudience"]),
+                //ValidAudience = ConfigurationManager.AppSettings["ida:Audience"],
+                AudienceValidator = ((audiences, jwt, validationParameters) =>
+                {
+                    // Write some code here to validate the audiences in the audiences parameter
+                    return true;
+                })
+            };
+            options.Provider = new OAuthBearerAuthenticationProvider()
+            {
+                OnValidateIdentity = context =>
+                {
+                    // Add custom claims here
+                    context.Ticket.Identity.AddClaim(
+                       new Claim(ClaimTypes.Role, "Admin"));
+                    return Task.FromResult(0);
+                }
+            };
+
+            app.UseWindowsAzureActiveDirectoryBearerAuthentication(options);
+        }
+    }
+}
---
+++ Separate.API/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Separate.API
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_EndRequest()
+        {
+            if (HttpContext.Current.Response.StatusCode == 401)
+            {
+                HttpContext.Current.Response.AddHeader("WWW-Authenticate", "authorization_uri=\"https://login.microsoftonline.com/nimccolldir.onmicrosoft.com\"");
+            }
+        }
+    }
+}
